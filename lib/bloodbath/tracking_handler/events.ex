c703f651805436c07178e854abd5677452d3fc22
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-06-16 21:10:33 +0200

    feat: execute only in production

--- lib/bloodbath/tracking_handler/events.ex
+++ lib/bloodbath/tracking_handler/events.ex
@@ -4,10 +4,20 @@ defmodule Bloodbath.TrackingHandler.Events do
     Slack
   }
 
+  defmacro dispatch(do: yield) do
+    quote do
+      if Mix.env() == :prod do
+        unquote(yield)
+      end
+    end
+  end
+
   def signup(person) do
-    Slack.send_message("New sign-up #{person.email}")
-    Mixpanel.track_event(person.id, "Sign up")
-    # Mixpanel.create_identity(person.id)
-    Mixpanel.set_profile(person.id, %{email: person.email, first_name: person.first_name, last_name: person.last_name})
+    dispatch do
+      Slack.send_message("New sign-up #{person.email}")
+      Mixpanel.track_event(person.id, "Sign up")
+      # Mixpanel.create_identity(person.id)
+      Mixpanel.set_profile(person.id, %{email: person.email, first_name: person.first_name, last_name: person.last_name})
+    end
   end
 end
