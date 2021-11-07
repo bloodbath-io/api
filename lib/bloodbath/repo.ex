f8fd9866ea8d14eeb44c882191f0ed8aedde515d
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-11-07 22:51:07 +0100

    perf: configuration change again

--- lib/bloodbath/repo.ex
+++ lib/bloodbath/repo.ex
@@ -2,8 +2,9 @@ defmodule Bloodbath.Repo do
   use Ecto.Repo,
     otp_app: :bloodbath,
     adapter: Ecto.Adapters.Postgres,
-    pool_size: 120,
+    pool_size: 100,
     timeout: 100_100_000,
     connect_timeout: 100_100_000,
-    queue_target: 5000
+    queue_target: 5000,
+    queue_interval: 5000
 end
