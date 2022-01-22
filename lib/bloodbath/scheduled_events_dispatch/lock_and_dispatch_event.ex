7202aac5a2e0ebeb68823b38d3be885cac3b9a62
Laurent Schaffner <laurent@livestorm.co>
2022-01-22 15:01:14 -0700

    fix: cancel response

--- lib/bloodbath/scheduled_events_dispatch/lock_and_dispatch_event.ex
+++ lib/bloodbath/scheduled_events_dispatch/lock_and_dispatch_event.ex
@@ -96,7 +96,7 @@ defmodule Bloodbath.ScheduledEventsDispatch.LockAndDispatchEvent do
       set_dispatched(event.id)
       # TODO: removed this temporarily to check what does the CPU burn
       # response = HTTPoison |> apply(event.method, arguments)
-      Logger.debug(%{resource: event.id, event: "Response received", payload: response})
+      # Logger.debug(%{resource: event.id, event: "Response received", payload: response})
       # # NOTE: this isn't going to work properly
       # # we should have an event stream to pipeline the response update in batch (kafka?)
       # # this spawns one connection each time it happens, and may delay the database connections
