83338c0df3b2106ae176be8229f668fbae12231c
Laurent Schaffner <laurent@livestorm.co>
2022-01-23 18:57:05 -0700

    chore: change pool size and dispatch limits

--- lib/bloodbath/scheduled_events_dispatch/lock_and_dispatch_event.ex
+++ lib/bloodbath/scheduled_events_dispatch/lock_and_dispatch_event.ex
@@ -77,7 +77,7 @@ defmodule Bloodbath.ScheduledEventsDispatch.LockAndDispatchEvent do
       # async: :once,
       timeout: 3_000, # time we keep connections alive -> always keep the connection slightly above
       recv_timeout: 2_000, # timeout on response
-      max_connections: 80
+      max_connections: 350
     ]
 
     arguments = [
