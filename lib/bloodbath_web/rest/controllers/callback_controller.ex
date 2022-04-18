a04ca24d586d90c2d7faf851240006568a9e6d7e
Laurent Schaffner <laurent@livestorm.co>
2022-04-18 11:54:38 -0600

    feat: callback ready

--- lib/bloodbath_web/rest/controllers/callback_controller.ex
+++ lib/bloodbath_web/rest/controllers/callback_controller.ex
@@ -1,9 +1,45 @@
 defmodule BloodbathWeb.CallbackController do
+  require Logger
   use BloodbathWeb, :controller
 
   action_fallback BloodbathWeb.FallbackController
 
+  alias Bloodbath.Repo
+  alias Bloodbath.CustomerEventsManagement.{
+    Event,
+    EventResponse
+  }
+
   def create(conn, params) do
+    event_id = params["id"]
+    event = Event |> Repo.get(event_id)
+
+    event |> set_response
+    insert_full_response(event, params["type"], params["body"], params["status"])
+
     render(conn, "index.json")
+  end
+
+  def set_response(event) do
+    Logger.debug(%{resource: event.id, event: "Updating response_received_at"})
+
+    event |> Event.update_changeset(%{response_received_at: Timex.now})
+    |> Repo.update!()
+  end
+
+  def insert_full_response(event, type, body, status) do
+    attrs = %{
+      type: type,
+      body: body,
+      headers: %{}, # yes this is useless
+      request_url: event.endpoint, # yes this is useless
+      status_code: status
+    }
+
+    Logger.debug(%{resource: event.id, event: "Ok received, we will create an EventResponse for it", data: attrs})
+
+    %EventResponse{} |> EventResponse.create_changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:event, event)
+    |> Repo.insert()
   end
 end
